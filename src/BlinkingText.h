2fd9f644397033cafdca29e3c39ac5f3f7d57013
KNProNooob <knpronoob@gmail.com>
2025-08-03 20:56:26 +0300

    Linux fix

--- src/BlinkingText.h
+++ src/BlinkingText.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 
 class BlinkingText : public sf::Text {
 public:
--- src/HPText.h
+++ src/HPText.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "SFML/Graphics.hpp"
-
+#include <SFML/Graphics.hpp>
 class Player;
 
 class HPText: public sf::Text{
